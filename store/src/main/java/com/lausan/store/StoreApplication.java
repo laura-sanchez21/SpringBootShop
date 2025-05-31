1b9fb666fe9d723b4bff5c28e2ea02821cf4d117
Laura Sanchez <slaura2048@gmail.com>
2025-05-31 14:49:54 -0700

    Added some dependency's, started implementing MVC pattern. Application is up and running with a basic html page.

--- store/src/main/java/com/lausan/store/StoreApplication.java
+++ store/src/main/java/com/lausan/store/StoreApplication.java
@@ -3,7 +3,7 @@ package com.lausan.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
- 
+ @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
