7c2858afdd13310be2b0d373ae556e17ab52e5e2
Laura Sanchez <slaura2048@gmail.com>
2025-06-14 23:03:43 -0700

    converted my application.properties to application.yaml

--- store/src/main/java/com/lausan/store/OrderService.java
+++ store/src/main/java/com/lausan/store/OrderService.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder() {
         paymentService.processPayment(10);
     }
-} 
+}
--- store/src/main/java/com/lausan/store/StoreApplication.java
+++ store/src/main/java/com/lausan/store/StoreApplication.java
@@ -1,6 +1,5 @@
 package com.lausan.store;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
